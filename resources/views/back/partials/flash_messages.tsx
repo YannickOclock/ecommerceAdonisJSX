f1fffdbd2609bbc4464e8e106fad22443f636f72
YannickOclock <yannick.kuhn@oclock.io>
2024-06-09 18:29:34 +0200

    feat(messages flash) : add warn message flash if login with no verified account

--- resources/views/back/partials/flash_messages.tsx
+++ resources/views/back/partials/flash_messages.tsx
@@ -1,20 +1,39 @@
 import { getFlashMessages } from '#resources/helpers/flash_messages'
 import clsx from 'clsx'
+import { HttpContext } from '@adonisjs/core/http'
 
-export function FlashMessages() {
+export async function FlashMessages() {
+  const { auth } = HttpContext.getOrFail()
+  await auth.check()
+
+  const authVerifiedMessage = !auth.user.verified && (
+    <div class={clsx(`alert alert-warning alert-dismissible`)} role="alert">
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+        <i class="material-icons">close</i>
+      </button>
+      <div class="alert-message">
+        <strong>Votre compte n'est pas activé</strong>,{' '}
+        <a href="#">renvoyer le lien d'activation</a>
+      </div>
+    </div>
+  )
+
   if (getFlashMessages().has('notification')) {
     {
       const notification = getFlashMessages().get('notification')
       return (
-        <div class={clsx(`alert alert-${notification.type} alert-dismissible`)} role="alert">
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-            <i class="material-icons">close</i>
-          </button>
-          <div class="alert-message">{notification.message}</div>
-        </div>
+        <>
+          {authVerifiedMessage && authVerifiedMessage}
+          <div class={clsx(`alert alert-${notification.type} alert-dismissible`)} role="alert">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+              <i class="material-icons">close</i>
+            </button>
+            <div class="alert-message">{notification.message}</div>
+          </div>
+        </>
       )
     }
   } else {
-    return <></>
+    return <>{authVerifiedMessage && authVerifiedMessage}</>
   }
 }
--- resources/views/front/layouts/base.tsx
+++ resources/views/front/layouts/base.tsx
@@ -1,14 +1,17 @@
 import { Footer } from '#viewsfront/partials/footer'
 import { Vite } from '#resources/helpers/asset'
 import { Nav } from '#viewsfront/partials/nav'
+import { HttpContext } from '@adonisjs/core/http'
 
 interface BaseProps {
   title?: string
   children: JSX.Element
 }
 
-export function Base(props: BaseProps) {
+export async function Base(props: BaseProps) {
   const { title = `Boutique`, children } = props
+  const { auth } = HttpContext.getOrFail()
+  await auth.check()
 
   return (
     <>
@@ -28,6 +31,24 @@ export function Base(props: BaseProps) {
         <body>
           {/*<Header />*/}
           <Nav />
+          {/*<Flash message />*/}
+          <div class="flash_messages">
+            {!!auth.user && !auth.user.verified && (
+              <div class="alert alert-warning alert-dismissible" role="alert">
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="alert"
+                  aria-label="Close"
+                ></button>
+                <div class="alert-message">
+                  <strong>Votre compte n'est pas activé</strong>,{' '}
+                  <a href="#">renvoyer le lien d'activation</a>
+                </div>
+              </div>
+            )}
+          </div>
+          {/*<Main />*/}
           <main up-main>{children}</main>
           <Footer />
         </body>
