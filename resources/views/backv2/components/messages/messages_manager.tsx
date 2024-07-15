a9df5738a1799b9c2783e99fe7eee7074d8286c6
YannickOclock <yannick.kuhn@oclock.io>
2024-07-15 21:30:29 +0200

    feat(back office integration) : show messages flash

---
+++ resources/views/backv2/components/messages/messages_manager.tsx
@@ -0,0 +1,15 @@
+import { getFlashMessages } from '#resources/helpers/flash_messages'
+import { SuccessMessage } from '#viewsbackv2/components/messages/success_message'
+
+export function MessagesManager() {
+  const messageElements = []
+
+  if (getFlashMessages().has('notification')) {
+    const notification = getFlashMessages().get('notification')
+    if (notification.type === 'success') {
+      messageElements.push(<SuccessMessage message={notification.message} />)
+    }
+  }
+
+  return <div class={'p-4'}>{messageElements.map((messageElement) => messageElement)}</div>
+}
