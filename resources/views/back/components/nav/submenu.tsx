993858ee3fa42091524f716f17467cc0be73e199
YannickOclock <yannick.kuhn@oclock.io>
2024-07-31 21:13:00 +0200

    feat(back office integration) : delete backoffice V1

--- resources/views/back/components/nav/submenu.tsx
+++
@@ -1,8 +0,0 @@
-interface SubMenuProps {
-  children?: JSX.Element
-}
-
-export function SubMenu(props: SubMenuProps) {
-  const { children } = props
-  return <ul class="submenu">{children}</ul>
-}
--- resources/views/back/partials/footer.tsx
+++
@@ -1,7 +0,0 @@
-export async function Footer() {
-  return (
-    <footer>
-      <p>© 2024 - Tous droits réservés</p>
-    </footer>
-  )
-}
